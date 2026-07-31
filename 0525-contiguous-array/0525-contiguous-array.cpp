7379f4128b64b64e9b3d68dc87a11c43cf265876
aditikhushi81-cpu <aditikhushi81@gmail.com>
2026-07-31 19:01:06 +0530

    Time: 22 ms (70.82%), Space: 88.2 MB (73.37%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+
+        unordered_map<int, int> mp;
+
+        // Prefix sum 0 exists before the array starts
+        mp[0] = -1;
+
+        int prefixSum = 0;
+        int maxLength = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            // Convert 0 to -1 and 1 to +1
+            if (nums[i] == 0)
+                prefixSum += -1;
+            else
+                prefixSum += 1;
+
+            // If this prefix sum appeared before
+            if (mp.find(prefixSum) != mp.end()) {
+
+                maxLength = max(maxLength, i - mp[prefixSum]);
+
+            }
+            else {
+
+                // Store only the first occurrence
+                mp[prefixSum] += i;
+            }
+        }
+
+        return maxLength;
+    }
+};
