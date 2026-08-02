36175773c1586977ebdaa7609306b14e3eb9306a
aditikhushi81-cpu <aditikhushi81@gmail.com>
2026-08-02 18:41:38 +0530

    Time: 0 ms (100%), Space: 9.9 MB (24.49%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+
+        unordered_map<int, int> freq;
+
+        int count = 0;
+
+        for (int num : nums) {
+
+            count += freq[num];
+
+            freq[num]++;
+        }
+
+        return count;
+    }
+};
