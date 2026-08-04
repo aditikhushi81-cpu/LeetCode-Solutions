8d70f1d88d2ca96b4df9e329a851b31802458786
aditikhushi81-cpu <aditikhushi81@gmail.com>
2026-08-04 18:09:57 +0530

    Time: 0 ms (100%), Space: 13.9 MB (35.68%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+
+        unordered_set<int> st;
+
+        for (int num : arr) {
+
+            if (st.find(2 * num) != st.end())
+                return true;
+
+            if (num % 2 == 0 && st.find(num / 2) != st.end())
+                return true;
+
+            st.insert(num);
+        }
+
+        return false;
+    }
+};
